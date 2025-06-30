5e448a6bb31917aab214042cfa36f7697ed2114a
ErickAguiar06 <erick.aguiar016@gmail.com>
2025-06-30 16:36:48 -0300

    att

--- api/server.js
+++ api/server.js
@@ -1,5 +1,16 @@
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
+const rota = require('./src/routes'); // ou './routes' dependendo de onde está
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(rota);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API rodando na porta ${PORT}`);
 });
